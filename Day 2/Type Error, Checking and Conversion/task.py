75551991990075f0e793a27ecba78f57d5a4bb2d
Nicole <nicolelau704@gmail.com>
2026-06-10 17:56:57 -0700

    Day 2

---
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -0,0 +1,14 @@
+#1
+#len("12345")
+
+#check the data type
+#print(type("abc"))
+
+#2
+#print(type(123))
+#print(type(123.45))
+#print(type(True))
+#print(type("abc"))
+
+#3
+print("Number of letters in your name: " + str(len(input("Enter your name\n"))))
