9564ab4cdb906204f96e64cedd53f39c94d80bfb
Nicole <nicolelau704@gmail.com>
2026-06-10 17:23:19 -0700

    Day 1

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,7 +1,11 @@
-#1
-print(len(input("What is your name?")))
+#Greeting
+print("Welcome to the Band Name Generator!\n")
 
-#2
-username = input("What is your name?")
-length = len(username)
-print(length)
+#Ask user for the city they grew up in
+city = input("What city did you grow up in?\n")
+
+#Ask user for the name of a pet
+pet = input("What is the name of your pet?\n")
+
+#Combine the name of the city and pet
+print("Your band name could be " + city + " " + pet)
