d15ee4d20e95cd2b163029ca5165841be0e1f99e
Nicole <nicolelau704@gmail.com>
2026-06-10 18:42:46 -0700

    Day 2

---
+++ Day 2/Number Manipulation/task.py
@@ -0,0 +1,25 @@
+bmi = 84 / 1.65 ** 2
+
+#rounds down
+print(int(bmi))
+
+#rounds up
+print(round(bmi))
+
+#rounds to 2 decimal places
+print(round(bmi, 2))
+
+#assignment
+score = 0
+
+#these do the same thing
+score = score + 1
+score += 1
+
+print(score)
+
+#f-Strings
+age = 12
+print(f"I am {age} years old")
+
+print(f"Your score is {score}")
