c60f5b0985ec0583f25247476a40e641982f197c
Nicole <nicolelau704@gmail.com>
2026-06-11 15:55:41 -0700

    Day 3

---
+++ Day 3/Python Pizza/task.py
@@ -0,0 +1,42 @@
+#Greeting
+print("Welcome to Python Pizza Deliveries!")
+
+#Get the user's pizza order
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+#Calculate the bill for the user's order
+#Check the size of the pizza and set the bill
+bill = 0
+if size == "S":
+    bill += 15
+
+    #Check what toppings the user wanted and add it to the bill
+    if pepperoni == "Y":
+        bill += 2
+    if extra_cheese == "Y":
+        bill += 1
+elif size =="M":
+    bill += 20
+
+    #Check what toppings the user wanted and add it to the bill
+    if pepperoni == "Y":
+        bill += 3
+    if extra_cheese == "Y":
+        bill += 1
+else:
+    bill += 25
+
+    #Check what toppings the user wanted and add it to the bill
+    if pepperoni == "Y":
+        bill += 3
+    if extra_cheese == "Y":
+        bill += 1
+
+#Display the total bill to the user
+print(f"The total bill for your pizza is ${bill}.")
+
+
+
+
