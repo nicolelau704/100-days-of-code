ba88513ca73e47de964dc0656b20ea6e7889cb96
Nicole <nicolelau704@gmail.com>
2026-06-11 14:50:49 -0700

    Day 3

--- Day 3/Modulo/task.py
+++ Day 3/Modulo/task.py
@@ -1,3 +1,14 @@
 print(6 % 2)
 print(6 % 5)
 print(6 % 4)
+
+#1
+print(10 % 3)
+
+#2 Check  to see if a user inputted number is odd or even
+user_number = int(input("Please type a number in\n"))
+
+if user_number % 2 == 0:
+    print("Your number is even")
+else:
+    print("Your number is odd")
