02495772d52fc4ebeee43cfcfe2e1317eefdb4cc
Nicole <nicolelau704@gmail.com>
2026-06-11 15:23:08 -0700

    Day 3

--- Day 3/Nesting and Elif/task.py
+++ Day 3/Nesting and Elif/task.py
@@ -6,13 +6,23 @@ height = int(input("What is your height in cm? "))
 
 #Check if the user 120cm tall or greater
 if height >= 120:
-    #Check if the user is at least 18 years old
+    #Ask for the user's age
     age = int(input("What is your age? "))
+
+    #Check if the user is at least 18 years old
     if age >= 18:
         print("You can ride the rollercoaster")
     else:
         #Tell user that they are not old enough
-        print("Sorry, you are not old enough to ride this rollercoaster.")
+        print("You can enter the park, but you are not old enough for the rollercoaster.")
+
+    #Tell the user how much their ticket costs based on their age
+    if age < 12:
+        print("Your ticket is $5.")
+    elif age < 18:
+        print("Your ticket is $7.")
+    else:
+        print("Your ticket is $12.")
 else:
-    #Tell user that they are not tall enough
-    print("Sorry you have to grow taller before you can ride.")
+    #Tell user that they are not tall enough for the rollercoaster
+    print("Sorry you have to grow taller before you can ride the rollercoaster.")
