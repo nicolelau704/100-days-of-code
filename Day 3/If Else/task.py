89a70431049e318e7674d2f35c295e56e139eec2
Nicole <nicolelau704@gmail.com>
2026-06-11 11:21:24 -0700

    Day 3

--- Day 3/If Else/task.py
+++ Day 3/If Else/task.py
@@ -1,6 +1,7 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
+#You must be over 120 cm tall to ride the rollercoaster
 if height > 120:
     print("You can ride the rollercoaster!")
 else:
