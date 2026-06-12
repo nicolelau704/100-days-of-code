4cd185f8118b8c5c1bbb850b9d58ca76d3777801
Nicole <nicolelau704@gmail.com>
2026-06-11 17:08:23 -0700

    Day 3

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -23,26 +23,26 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
-choice = input("You've reached a fork in the road. Would you like to go left or right? (left/right) ")
-if choice == "right" or choice == "Right":
+choice = input("You've reached a fork in the road. Would you like to go left or right? (left/right) ").lower()
+if choice == "right":
     print("You fell into a hole! Game over.")
-elif choice == "left" or choice == "Left":
+elif choice == "left":
     print("A huge lake appears just beyond the line of trees.\n"
           "You can see a small island floating in the middle.\n")
-    choice = input("Do you choose to swim across or wait for a boat? (swim/wait) ")
-    if choice == "swim" or choice == "Swim":
+    choice = input("Do you choose to swim across or wait for a boat? (swim/wait) ").lower()
+    if choice == "swim":
         print("A trout surges upward and attacks you! Game over.")
-    elif choice == "wait" or choice == "Wait":
+    elif choice == "wait":
         print("A woman pulls up in a fishing boat an hour later.\n"
               "She tells you that the island is rumored to have treasure on it.\n"
               "She offers to take you there if you give her a portion of the treasure.\n"
               "She takes you across.You explore for a while until you come across a passageway with 3 doorways.")
-        choice = input("Which door do you choose? (red/yellow/blue) ")
-        if choice == "red" or choice == "Red":
+        choice = input("Which door do you choose? (red/yellow/blue) ").lower()
+        if choice == "red":
             print("You trigger a booby trap and get burned by fire! Game over.")
-        elif choice == "yellow" or choice == "Yellow":
+        elif choice == "yellow":
             print("Piles of gold lay all around. You win!")
-        elif choice == "blue" or choice == "Blue":
+        elif choice == "blue":
             print("You hear a loud growl and don't have time to react before a beast lunges upon you. Game over.")
         else:
             print("That is the wrong input. Game over.")
