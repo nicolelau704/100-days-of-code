1cd558f851e3d0dd35c89524e08af1ccaeba6075
Nicole <nicolelau704@gmail.com>
2026-06-13 00:07:06 -0700

    Day 5

--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,7 +1,7 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
 print(range(1, 10))
 
-#Use Python's build in sum() function
+#Use Python's built in sum() function
 total_exam_scores = sum(student_scores)
 print(total_exam_scores)
 
