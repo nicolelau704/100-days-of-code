5a56173cc7d84473cb0b0d1c06d0f1c56211ce65
Nicole <nicolelau704@gmail.com>
2026-06-12 23:15:37 -0700

    Day 5

--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,1 +1,13 @@
 fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
+
+for fruit in fruits:
+    print(fruit)
+    print("Hello")
+
+for fruit in fruits:
+    print(fruit)
+
+print("Hello")
