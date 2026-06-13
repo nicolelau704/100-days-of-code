9c37f3bec774b51cfac59750aaae30e378902162
Nicole <nicolelau704@gmail.com>
2026-06-12 17:44:02 -0700

    Day 4

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,10 @@
+import random
+
+#list of friend names
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+which_friend = random.randint(0,4)
+
+#Pick a random name from the list of friends
+print(friends[which_friend])
+
