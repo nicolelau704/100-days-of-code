22d4c02b9f35808119d1aa5a7debfa54b68b2487
Nicole <nicolelau704@gmail.com>
2026-06-13 17:54:25 -0700

    Day 6

---
+++ Day 6/ReeborgsMaze.py
@@ -0,0 +1,22 @@
+#Uses website reeborg.ca to go through a maze
+#
+#There is no way to turn the robot right, so create a function
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#Make sure you start next to a wall on the right side
+# while front_is_clear():
+#     move()
+# turn_left() #puts wall on right
+#
+#Keep touching a wall on the right side as you maneuver through the maze
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
