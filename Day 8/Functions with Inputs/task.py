cb7b4e4d6fb0af9b4d8d457905b7cd1b30530e8e
Nicole <nicolelau704@gmail.com>
2026-06-17 01:58:52 -0700

    Day 8

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,14 @@
+def greet():
+    print("Hello Nicole")
+    print("Welcome Flynn")
+    print("What's up, Teddy?")
+
+greet()
+
+#using parameters and arguments with functions
+def my_function(name):
+    print(f"Hello {name}")
+    print(f"Welcome {name}")
+    print(f"What's up, {name}?")
+
+my_function("Nicole")
