90657c32bfacc7be1ddc006d32e8b54cb06e9e29
Nicole <nicolelau704@gmail.com>
2026-06-17 12:02:30 -0700

    Day 8

--- Day 8/Positional vs Keyword Arguments/Love_Calculator.py
+++ Day 8/Positional vs Keyword Arguments/Love_Calculator.py
@@ -1,3 +1,5 @@
+#Test the compatability between two names
+
 def calculate_love_score(name1, name2):
     name1.lower()
     name2.lower()
@@ -9,14 +11,17 @@ def calculate_love_score(name1, name2):
     true_list = ["t", "r", "u", "e"]
     love_list = ["l", "o", "v", "e"]
 
+    #Count how many times the letters from "TRUE" appear among both names
     for letter in name3:
         if letter in true_list:
             true_count += 1
 
+    #Count how many times the letters from "LOVE" appear among both names
     for letter in name3:
         if letter in love_list:
             love_count += 1
 
+    #Concatenate the first number and second number together
     print(str(true_count) + str(love_count))
 
 
