68605475d2a9bd931beb738fd000605a2f29045a
Nicole <nicolelau704@gmail.com>
2026-06-17 15:20:52 -0700

    Day 8

---
+++ Day 8/Caesar Cipher 2/main.py
@@ -0,0 +1,56 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+# 1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
+def decrypt(original_text, shift_amount):
+    # 2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
+    #  by the shift amount and print the decrypted text.
+    deciphered_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) - shift_amount
+        shifted_position %= len(alphabet)
+        deciphered_text += alphabet[shifted_position]
+    print(f"Here is the decoded result: {deciphered_text}")
+
+def encrypt(original_text, shift_amount):
+    cipher_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        cipher_text += alphabet[shifted_position]
+    print(f"Here is the encoded result: {cipher_text}")
+
+# 3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
+#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
+def caesar(direction_code, original_text, shift_amount):
+    if direction_code == "encode":
+        cipher_text = ""
+
+        for letter in original_text:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            cipher_text += alphabet[shifted_position]
+
+        print(f"Here is the encoded result: {cipher_text}")
+
+    elif direction_code == "decode":
+        deciphered_text = ""
+
+        for letter in original_text:
+            shifted_position = alphabet.index(letter) - shift_amount
+            shifted_position %= len(alphabet)
+            deciphered_text += alphabet[shifted_position]
+
+        print(f"Here is the decoded result: {deciphered_text}")
+
+    else:
+        print("Sorry, that's not a valid response.")
+
+
+#encrypt(original_text=text, shift_amount=shift)
+#decrypt(original_text=text, shift_amount=shift)
+caesar(direction, text, shift)
