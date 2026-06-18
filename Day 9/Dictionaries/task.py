800f8d468c97c90b6f468eb3da252d2467974ee4
Nicole <nicolelau704@gmail.com>
2026-06-17 17:25:39 -0700

    Day 9

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,16 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
+                          "Function": "A piece of code that you can easily call over and over again."
+}
+
+#create a dictionary
+colours = { "apple": "red",
+            "pear" : "green",
+            "banana" : "yellow"
+}
+
+print(colours["pear"])      #retrieve items from a dictionary
+
+my_empty_dictionary = {}    #create an empty dictionary
+
+colours["peach"] = "pink"   #add to dictionary
+colours["apple"] = "maroon" #change dictionary entry
