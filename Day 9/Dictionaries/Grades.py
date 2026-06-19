d32a39c8d6335054d90449a8e28b024bdf00e739
Nicole <nicolelau704@gmail.com>
2026-06-18 18:30:15 -0700

    Day 9

---
+++ Day 9/Dictionaries/Grades.py
@@ -0,0 +1,25 @@
+#Dictionary holds each student's test scores
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {}
+
+#Assign each student a grade assessment based on their test scores
+for student in student_scores:
+    if student_scores[student] >= 91:
+        student_grades[student] = "Outstanding"
+    elif student_scores[student] >= 81:
+        student_grades[student] = "Exceeds Expectations"
+    elif student_scores[student] >= 71:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+#Print out the student name and the assessed grade
+for student in student_grades:
+    print(student + " " + student_grades[student])
