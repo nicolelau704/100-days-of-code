41c92669684681604d6f9350e2d70e6767ae7991
Nicole <nicolelau704@gmail.com>
2026-06-18 18:56:54 -0700

    Day 9

--- Day 9/Nested Lists and Dictionaries/task.py
+++ Day 9/Nested Lists and Dictionaries/task.py
@@ -1,10 +1,29 @@
-travel_log = {
-    "France": ["Paris", "Lille", "Dijon"],
-    "Germany": ["Stuttgart", "Berlin"],
-}
+#Create a Dictionary with a List nested inside for the values
+# travel_log = {
+#     "France": ["Paris", "Lille", "Dijon"],
+#     "Germany": ["Stuttgart", "Berlin"],
+# }
 
-print(travel_log["France"][1])
+#Print "Lille"
+#print(travel_log["France"][1])
 
+#Create a nested List inside a List
 nested_list = ["A", "B", ["C", "D"]]
 
-print(nested_list[2][1])
+#Print "D"
+#print(nested_list[2][1])
+
+#Create a dictionary with a nested dictionary
+travel_log = {
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 8
+    },
+}
+
+#Print "Stuttgart"
+print(travel_log["Germany"]["cities_visited"][2])
