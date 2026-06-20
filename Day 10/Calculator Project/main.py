dd12aba2cdff04210b8668b5ae0d90c7f32461f1
Nicole <nicolelau704@gmail.com>
2026-06-20 12:52:43 -0700

    Day 10

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,7 +1,5 @@
 import art
 
-print(art.logo)
-
 def add(n1, n2):
     return n1 + n2
 
@@ -32,27 +30,42 @@ calculations = {
     "/": divide,
 }
 
-first_num = 0.0
-second_num = 0.0
-answer = ""
-calculating = True
-continue_calc = "n"
-while calculating:
-    if answer != "":
-        continue_calc = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation. ")
+def calculator():
+    #Display the logo at the beginning of the calculator
+    print(art.logo)
 
-    operation = input("+\n-\n*\n/\nPick an operation: ")
+    #Get the first number from the user
+    first_num = float(input("What is the first number? "))
 
-    if operation not in calculations:
-        print("Sorry, that is not a correct operation. Default operation is '+'")
-        operation = "+"
-    if continue_calc == "y":
-        first_num = answer
+    calculating = True
+    while calculating:
+        #Ask the user for the operation
+        operation = input("+\n-\n*\n/\nPick an operation: ")
+
+        #Restart the calculator if the operation is not valid
+        if operation not in calculations:
+            print("Sorry, that is not a correct operation.")
+            calculator()
+
+        #Ask the user for the second number
         second_num = float(input("What is the second number? "))
-        answer = calculations[operation](answer, second_num)
-    else: #continue_calc == "n":
-        first_num = float(input("What is the first number? "))
-        second_num = float(input("What is the second number? "))
+
+        #Get the answer
         answer = calculations[operation](first_num, second_num)
 
-    print(format_answer(operation, first_num, second_num))
+        #Display the answer
+        print(format_answer(operation, first_num, second_num))
+
+        #Ask user if they want to continue the calculation with the previous answer or start a new one
+        continue_calc = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation. ")
+
+        #Continue the calculation using the previous answer as the first number
+        if continue_calc == "y":
+            first_num = float(answer)
+        #Start a new calculation by calling the same function again to make it start over
+        else:
+            calculating = False
+            print("\n" * 50)
+            calculator()
+
+calculator()
