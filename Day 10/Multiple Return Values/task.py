a244c5cc7a26e87744d7a959472a4ea741cc888e
Nicole <nicolelau704@gmail.com>
2026-06-19 23:16:13 -0700

    Day 10

---
+++ Day 10/Multiple Return Values/task.py
@@ -0,0 +1,9 @@
+#The function can have multiple return statements when there is a conditional if statement
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You did not provide a valid name."
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
