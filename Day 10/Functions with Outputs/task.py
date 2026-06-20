edfb3ddbfa9958f7cdb388d5c3cd235bb41dcc11
Nicole <nicolelau704@gmail.com>
2026-06-19 22:37:08 -0700

    Day 10

---
+++ Day 10/Functions with Outputs/task.py
@@ -0,0 +1,18 @@
+#Function that prints data
+def format_name(f_name, l_name):
+    first_name = f_name.title()
+    last_name = l_name.title()
+    output_string = first_name + " " + last_name
+
+    print(output_string)
+
+#Function that returns data
+def format_name2 (f_name, l_name):
+    first_name = f_name.title()
+    last_name = l_name.title()
+    output_string = first_name + " " + last_name
+
+    return output_string
+
+format_name("flynn","dog")
+print(format_name2("dash", "dog"))
