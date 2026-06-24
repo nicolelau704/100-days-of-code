aa99864402f10e65ac7ab90acf6073564d661e2f
Nicole <nicolelau704@gmail.com>
2026-06-23 18:32:12 -0700

    Day 13

---
+++ Day 13/Describe the Problem/task.py
@@ -0,0 +1,16 @@
+def my_function():
+    for i in range(1, 21):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
+
+# Describe the Problem - Write your answers as comments:
+    #The function is supposed to print "You got it" when it reaches 20, but it's not printing anything
+# 1. What is the for loop doing?
+    #It's incrementing the variable i by 1 from 1 to 20
+# 2. When is the function meant to print "You got it"?
+    #when i reaches 20
+# 3. What are your assumptions about the value of i?
+    #i may not ever equal 20
