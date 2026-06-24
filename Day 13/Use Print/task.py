62305425a33a221ad7564aa54175183031c0b328
Nicole <nicolelau704@gmail.com>
2026-06-23 19:15:51 -0700

    Day 13

---
+++ Day 13/Use Print/task.py
@@ -0,0 +1,6 @@
+pages = int(input("Number of pages: "))
+#print(f"pages is {pages}")
+word_per_page = int(input("Number of words per page: "))
+#print(f"word_per_page is {word_per_page}")
+total_words = pages * word_per_page
+print(f"The total number of words is {total_words}.")
