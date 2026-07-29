a848f64faf8144b34d9831933af34a99a5c3c08c
Nicole <nicolelau704@gmail.com>
2026-07-28 22:41:26 -0700

    Day 20

--- Day 20/snake-game/main.py
+++ Day 20/snake-game/main.py
@@ -22,14 +22,16 @@ for position in starting_positions:
 #Make the snake move
 game_is_on = True
 while game_is_on:
+    #Move the snake in the background then update the screen every 0.1 seconds so the snake has a smooth movement
     screen.update()
     time.sleep(0.1)
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
+    #Move each body segment to the body segment before its position. Move the 1st to a new position
+    for seg_num in range(len(segments) - 1, 0, -1):     #starts with position of last segment
+        new_x = segments[seg_num - 1].xcor()    #gets x coordinate for segment before it
+        new_y = segments[seg_num - 1].ycor()    #gets y coordinate for segment before it
+        segments[seg_num].goto(new_x, new_y)    #moves the segment to the position of the segment before it
 
-    segments[0].forward(20)
+    segments[0].forward(20)     #moves segment 1
 
 
 screen.exitonclick()
