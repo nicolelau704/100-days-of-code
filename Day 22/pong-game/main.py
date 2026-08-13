fd0993e59db9f7050505f1ffdbddccff204033ed
Nicole <nicolelau704@gmail.com>
2026-08-12 18:16:20 -0700

    Day 22

--- Day 22/pong-game/main.py
+++ Day 22/pong-game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 import time
 
 #Create screen
@@ -13,16 +14,39 @@ screen.tracer(0)
 right_paddle = Paddle((350,0))
 left_paddle = Paddle((-350,0))
 
+#Create ball
+ball = Ball()
+
 #Allow the program to accept inputs from the user to control the paddle's movement
 screen.listen()
 screen.onkey(fun=right_paddle.up,key="Up")
 screen.onkey(fun=right_paddle.down,key="Down")
 screen.onkey(fun=left_paddle.up,key="w")
 screen.onkey(fun=left_paddle.down,key="s")
-#Make the paddle move
+
+#Start the game
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+
+    ball.move()
+
+    #Detect if the ball collides with the top or bottom of the screen
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #Detect if the ball collides with a paddle
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 330 or ball.distance(left_paddle) < 50 and ball.xcor() < -330:
+        ball.bounce_x()
+
+    #Detect if right paddle misses
+    if ball.xcor() > 400:
+        ball.reset_ball()
+
+    #Detect if left paddle misses
+    if ball.xcor() < -400:
+        ball.reset_ball()
+
 
 screen.exitonclick()
