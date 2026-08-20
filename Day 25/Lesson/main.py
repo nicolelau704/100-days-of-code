450d53a75d0c3f27d5e65133a4327ac35d52eaf8
Nicole <nicolelau704@gmail.com>
2026-08-20 12:35:05 -0700

    Day 25

--- Day 25/Lesson/main.py
+++ Day 25/Lesson/main.py
@@ -21,4 +21,52 @@
 #Use Pandas to open, read, and display information from the csv file
 import pandas
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+
+#print just a column of the data
+# print(data["temp"])
+
+#save the data as a dictionary
+# data_dict = data.to_dict()
+# print(data_dict)
+
+#save the data from a column as a list
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+
+#Find the average temperature
+# sum = 0
+# for temp in temp_list:
+#     sum += temp
+#
+# average = round(sum / len(temp_list))
+# print(f"The average temperature is {average}")
+
+#alternate way to find the average
+# print(data["temp"].mean())
+
+#Find the maximum temperature
+# print(data["temp"].max())
+
+#print a row of data
+# print(data[data.day =="Monday"])
+
+#print the row with the highest temperature
+# highest = data["temp"].max()
+# print(data[data.temp == highest])
+
+#print the value of something from the row
+# monday = data[data.day == "Monday"]
+# print(monday.condition)
+
+#convert monday's temperature to fahrenheit
+# temp = (monday.temp * (9/5)) + 32
+# print(temp)
+
+#create a dataframe from scratch
+data_dict = {
+    "students": ['Amy', 'James', 'Angela'],
+    "scores": [76, 56, 65]
+}
+
+data2 = pandas.DataFrame(data_dict)
+data2.to_csv("new_data.csv")
